6e6900cc943434245f0e2af78a26489d93bd02c6
ericshen9999 <dragoneshen@gmail.com>
2021-10-11 22:34:30 -0700

    Update main.js
    Base Code

--- docs/baseball/main.js
+++ docs/baseball/main.js
@@ -1,13 +1,65 @@
-title = "";
+title = "Baseball";
 
-description = `
+description = ` [Press] to swing
 `;
 
-characters = [];
+characters = [
+  `
+  ll
+  ll
+  `,
+  `
+  l
+  `
+];
 
 options = {};
 
+let ballx;
+let bally;
+let ballspeed;
+let strike;
 function update() {
   if (!ticks) {
+    ballx = 20;
+    bally = 80;
+    ballspeed = 0.7;
+    strike = 0;
+  }
+  //Ball
+  char('a',ballx,bally);
+
+  //Temp
+  char('b',70,80);
+  char('b',80,80);
+
+  //Check if hit ball
+  if(input.isJustPressed){
+    if(ballx >= 70 && ballx <= 80){
+      // Increase score, speed, and reset position
+      score++;
+      ballspeed += 0.1;
+      ballx = 20;
+    } else {
+      // Increase strike and reduce ball speed by 1 (min 0.7)
+      strike++;
+      ballspeed -= 1;
+      if(ballspeed < 0.7){
+        ballspeed = 0.7;
+      }
+      ballx = 20;
+    }
+  }
+  //Move ball across the screen
+  if(ballx > 100){
+    // If ball is missed
+    strike++;
+    ballx = 20;
+  } else {
+    ballx += ballspeed;
+  }
+  //3 strikes
+  if(strike >= 3){
+    end();
   }
 }
