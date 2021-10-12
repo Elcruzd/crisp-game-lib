4637196a9b94fa23c5e90256bfe6f7da01a350aa
ericshen9999 <dragoneshen@gmail.com>
2021-10-12 14:52:52 -0700

    Added Strike Symbols

--- docs/baseball/main.js
+++ docs/baseball/main.js
@@ -10,6 +10,11 @@ characters = [
   `,
   `
   l
+  `,
+  `
+  l l
+   l
+  l l
   `
 ];
 
@@ -19,47 +24,67 @@ let ballx;
 let bally;
 let ballspeed;
 let strike;
+let delay;
 function update() {
   if (!ticks) {
     ballx = 20;
     bally = 80;
     ballspeed = 0.7;
     strike = 0;
+    delay = 0;
+  }
+
+  //Strike display
+  switch(strike){
+    case 3:
+      char('c',90,10);
+    case 2:
+      char('c',85,10);
+    case 1:
+      char('c',80,10);
+  }
+  //3 strikes
+  if(strike >= 3){
+    end();
+  }
+
+  //Delay before next pitch
+  if(delay > 0){
+    delay--;
   }
   //Ball
   char('a',ballx,bally);
 
-  //Temp
+  //Temp markers
   char('b',70,80);
   char('b',80,80);
 
   //Check if hit ball
   if(input.isJustPressed){
     if(ballx >= 70 && ballx <= 80){
-      // Increase score, speed, and reset position
+      //Increase score, speed, and reset position
       score++;
       ballspeed += 0.1;
-      ballx = 20;
     } else {
-      // Increase strike and reduce ball speed by 1 (min 0.7)
+      //Increase strike and reduce ball speed by 1 (min 0.7)
       strike++;
       ballspeed -= 1;
-      if(ballspeed < 0.7){
-        ballspeed = 0.7;
+      if(ballspeed < 0.3){
+        ballspeed = 0.3;
       }
-      ballx = 20;
     }
+    ballx = 20;
+    //Remove delay
+    delay = 10;
   }
   //Move ball across the screen
   if(ballx > 100){
-    // If ball is missed
+    //If ball is missed
     strike++;
     ballx = 20;
   } else {
-    ballx += ballspeed;
-  }
-  //3 strikes
-  if(strike >= 3){
-    end();
+    if(delay == 0){
+      ballx += ballspeed;
+    }
   }
 }
